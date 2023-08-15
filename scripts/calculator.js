6b4301df236a35532596e91bb643c4a23d518855
amalthomas-exe <thomasamal479@gmail.com>
2023-08-15 23:29:21 +0530

    calculator done

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,8 +1,38 @@
- const radioInputs = document.querySelectorAll('input[type=radio][name="marital"]')
- radioInputs.forEach(input => {
+let score = 0; 
+
+ const maritalRadioInputs = document.querySelectorAll('input[type=radio][name="marital"]')
+ maritalRadioInputs.forEach(input => {
      input.addEventListener('change', function() {
-         if (this.checked) {
-             console.log("Selected value:", this.value);
+         if (this.value === 'married') {
+             document.getElementById('marital-accompanying').style.display = 'block';
+         }
+         else{
+            document.getElementById('marital-accompanying').style.display = 'none';
          }
      });
  });
+
+ const spouseAccompanyingRadioInputs = document.querySelectorAll('input[type=radio][name="marital-accompanying"]')
+    spouseAccompanyingRadioInputs.forEach(input => {
+        input.addEventListener('change', function() {
+            if (this.value === 'marital-accompanying-yes') {
+                console.log('yes');
+                document.getElementById('partner-details').style.display = 'block';
+            }
+            else{
+                document.getElementById('partner-details').style.display = 'none';
+            }
+        });
+    });
+
+const canadaJobOfferRadioInputs = document.querySelectorAll('input[type=radio][name="canada-empl-offer"]')
+canadaJobOfferRadioInputs.forEach(input => {
+    input.addEventListener('change', function() {
+        if (this.value === 'canada-empl-offer-yes') {
+            document.getElementById('job-type').style.display = 'block';
+        }
+        else{
+            document.getElementById('job-type').style.display = 'none';
+        }
+    });
+});
